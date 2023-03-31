99f7ad064a2f9478ba21fb0662bced548f2a7e35
Anna Carvalho <anna.escbr@gmail.com>
2023-03-31 00:00:25 -0300

    Update LISTAENC.cpp

--- LISTAENC.cpp
+++ LISTAENC.cpp
@@ -1,1 +1,18 @@
-aaa
+#include <iostream>
+using namespace std;
+
+struct no{
+public:
+int info; no *next;
+};
+
+struct no *head = NULL;
+
+void inserir(int conteudo){
+  no *novo = new no;
+  novo->info = conteudo;
+  *novo->next = *head;
+  *head = *novo;
+  cout << "Inserido no começo!"<<endl;
+  cout << conteudo << endl;
+}
